01ed996b4b4a554ab5d831a77eb5aef81893dbb3
Ahimcsatai <csataiahim@gmail.com>
2023-12-13 10:27:27 +0100

    egy sor törölve

--- orai.py
+++ orai.py
@@ -2,7 +2,6 @@ a=input("Mi a neved? ")
 b=input("Hány éves vagy? ")
 print("A maga életkora ",b,"és az ön neve ",a)
 b=int
-print("Ilyenkor született",2023-b)
 s=int(input("milyen indős anyukád? "))
 g=int(input("milyen indős apukád? "))
 print("anyukád ",s-b," volt mikor megszülettél")
